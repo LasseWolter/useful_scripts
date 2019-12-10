50f2de2f966f0281ec44bef3e798c53d47cd7067
Lasse Wolter <wolter.lasse@gmail.com>
2019-12-11 00:17:47 +0100

    Changed minor things in coc install script

--- vim/install_coc.sh
+++ vim/install_coc.sh
@@ -5,7 +5,7 @@ set -o errexit    # exit when command fails
 
 # Install latest nodejs
 if [ ! -x "$(command -v node)" ]; then
-    curl --fail -LSs https://install-node.now.sh/latest | sh
+    curl --fail -LSs https://install-node.now.sh/latest | sudo bash
     export PATH="/usr/local/bin/:$PATH"
     # Or use apt-get
     # sudo apt-get install nodejs
@@ -14,13 +14,13 @@ fi
 # Use package feature to install coc.nvim
 
 # for vim8
-mkdir -p ~/.vim/pack/coc/start
-cd ~/.vim/pack/coc/start
-curl --fail -L https://github.com/neoclide/coc.nvim/archive/release.tar.gz | tar xzfv -
+#mkdir -p ~/.vim/pack/coc/start
+#cd ~/.vim/pack/coc/start
+#curl --fail -L https://github.com/neoclide/coc.nvim/archive/release.tar.gz | tar xzfv -
 # for neovim
-# mkdir -p ~/.local/share/nvim/site/pack/coc/start
-# cd ~/.local/share/nvim/site/pack/coc/start
-# curl --fail -L https://github.com/neoclide/coc.nvim/archive/release.tar.gz | tar xzfv -
+mkdir -p ~/.local/share/nvim/site/pack/coc/start
+cd ~/.local/share/nvim/site/pack/coc/start
+curl --fail -L https://github.com/neoclide/coc.nvim/archive/release.tar.gz | tar xzfv -
 
 # Install extensions
 mkdir -p ~/.config/coc/extensions
