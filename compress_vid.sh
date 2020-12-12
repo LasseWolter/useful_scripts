72458e4e99405955c6accb6abba58694833096d2
Lasse Wolter <wolter.lasse@gmail.com>
2020-12-12 02:01:22 +0100

    Updated compress_vid to include usage print

--- compress_vid.sh
+++ compress_vid.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # Command taken from:
 # https://unix.stackexchange.com/questions/28803/how-can-i-reduce-a-videos-size-with-ffmpeg
+
+if [ -z "$1" ];
+then
+    echo "Usage: ./compress.vid.sh <input file> <output file>" 
+    exit 1
+fi
 echo "--------------------------------------------------"
 echo "Creating compressed version of $1 with new name $2"
 echo "--------------------------------------------------"
