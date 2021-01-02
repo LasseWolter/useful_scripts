c2b07e663e549282ea7f8055b223d13b7f4fb69f
Lasse Wolter <wolter.lasse@gmail.com>
2021-01-02 14:19:10 +0100

    Added plasmashell restart at the end + input names changed
    + input names are only relevant for my particular setup
    + but the plasmshell restart might be useful in general bc plasmashell
      crashed regularly when using the xrandr commands

--- extend_screen.sh
+++ extend_screen.sh
@@ -2,5 +2,9 @@
 # Simple script to turn on second screen connected over DP and 
 # arrange them according to my current setup
 # For a different setup --ouputs and --pos arguments will likely have to be changed
-xrandr --output DP-2-8 --pos 1920x0 --auto 
-xrandr --output DP-3-1 --pos 0x120 --auto 
+xrandr --output DP2-8 --pos 1920x0 --auto 
+xrandr --output DP3-1 --pos 0x120 --auto 
+
+# The extension kinda throws up plasmashell sometimes, so we just restart it
+# once the extension is done
+plasmashell --replace &
