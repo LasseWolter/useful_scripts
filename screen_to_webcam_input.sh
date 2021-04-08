65b08147a912b7a5f3c3b31aa2b17fc3b2949f04
Lasse Wolter <wolter.lasse@gmail.com>
2021-04-08 12:49:38 +0200

    screen_to_webcam_input + add video device automatically
    + if there is no video device present - create a dummy video device
    + because /dev/video0 is not a regular file [-f] will evaluate it to false
      [-e] however evaluates to true

--- screen_to_webcam_input.sh
+++ screen_to_webcam_input.sh
@@ -10,6 +10,18 @@
 # 1) Check which video devices are available before running that command using 'll /dev/video*'
 # 2) Create new video device with 'modprobe v4l2loopback'
 
-# /dev/video1 might need to be changed to the video device created with 'modprobe v4l2loopback'
+# Create dummy video device if none exists
+# We assume that no other video device is registered - this might need to be changed
+# in that case /dev/video0 needs to be replaced with /dev/videoN
+FILE=/dev/video0
+echo $FILE
+if [ ! -e "$FILE" ]; then
+    echo "$FILE does not exist.."
+    echo "Creating new dummy video device..."
+    sudo modprobe v4l2loopback
+fi
+
+# /dev/videoN might need to be changed to the video device created with 'modprobe v4l2loopback'
 # -s flag stands for the resolution and should be adjusted accordingly
-ffmpeg -f x11grab -r 15 -s 1280x720 -i :0.0+0,0 -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0
+# -i :0.0+x,y stands for the offset and might need to be adjusted accordingly 
+ffmpeg -f x11grab -r 40 -s 1920x1080 -i :0.0+1280,0 -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0
