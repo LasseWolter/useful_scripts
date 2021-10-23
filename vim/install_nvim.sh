0ff6521154d9fee28f6b60072f1fd3f75dcf1ea9
Lasse Wolter <wolter.lasse@gmail.com>
2021-10-23 15:03:04 +0100

    [scripts/vim] Updated nvim install script

--- vim/install_nvim.sh
+++ vim/install_nvim.sh
@@ -2,17 +2,32 @@
 
 old_dir="$PWD"
 
+function install_nvim() {
+  wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage -O nvim.appimage
+  chmod +x nvim.appimage
+  mkdir -p ~/.config/nvim/
+  cat << EOM > ~/.config/nvim/init.vim
+  set runtimepath^=~/.vim runtimepath+=~/.vim/after
+  let &packpath=&runtimepath
+  source ~/.vimrc 
+EOM
+}
+
 mkdir -p ~/AppImages
 cd ~/AppImages
 
 if [ -f "nvim.appimage" ]; then
-  echo "nvim.appimage exists. Would you like to override?  - yes/[no]"
-  read answer
+  echo "nvim.appimage exists. Would you like to override?"
+  echo "Note: This will also create a .config/nvim/init.vim file to use your .vimrc-file"
+  echo "Override: yes/[no]"
+  read answer; 
+else 
+  install_nvim
 fi
 
-if [ $answer == "yes" ]; then
-  wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage -O nvim.appimage
-  chmod +x nvim.appimage
+if [[ $answer == "yes" ]]; then
+  install_nvim
 fi
+
 
 cd $old_dir
