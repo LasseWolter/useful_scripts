38b2972916e5a8683ec9da7df74bc4419fa16ada
Lasse Wolter <wolter.lasse@gmail.com>
2023-01-02 16:18:59 +0000

    Added update-alternatives script + installation steps in README

---
+++ vim/nvim-update-alternatives.sh
@@ -0,0 +1,9 @@
+# The following commands update the default paths:
+# Set the above with the correct path, then run the rest of the commands:
+CUSTOM_NVIM_PATH=~/AppImages/nvim.appimage
+set -u
+sudo update-alternatives --install /usr/bin/ex ex "${CUSTOM_NVIM_PATH}" 110
+sudo update-alternatives --install /usr/bin/vi vi "${CUSTOM_NVIM_PATH}" 110
+sudo update-alternatives --install /usr/bin/view view "${CUSTOM_NVIM_PATH}" 110
+sudo update-alternatives --install /usr/bin/vim vim "${CUSTOM_NVIM_PATH}" 110
+sudo update-alternatives --install /usr/bin/vimdiff vimdiff "${CUSTOM_NVIM_PATH}" 110
