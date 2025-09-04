1a11a1ac207721c3669661ccb49e594d958a398a
Lasse Wolter <wolter.lasse@gmail.com>
2025-09-04 11:01:04 +0100

    feat(p10k): Move py venv to beginning of line

--- shell_setup/omzsh/p10k.zsh
+++ shell_setup/omzsh/p10k.zsh
@@ -39,6 +39,7 @@
     # =========================[ Line #2 ]=========================
     newline                 # \n
     pyenv                   # python environment (https://github.com/pyenv/pyenv)
+    virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
     # prompt_char           # prompt symbol
   )
 
@@ -53,7 +54,6 @@
     background_jobs         # presence of background jobs
     direnv                  # direnv status (https://direnv.net/)
     asdf                    # asdf version manager (https://github.com/asdf-vm/asdf)
-    virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
     anaconda                # conda environment (https://conda.io/)
     goenv                   # go environment (https://github.com/syndbg/goenv)
     nodenv                  # node.js version from nodenv (https://github.com/nodenv/nodenv)
